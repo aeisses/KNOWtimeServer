5cdc9f0f3a6742cdf18e96812d67e941df3e2778
Aaron Eisses <aaron.eisses@gmail.com>
2014-10-18 17:23:01 -0300

    Fixing merge issues

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require('express')
 var app = express();
 var pg = require('pg');
 
-<<<<<<< HEAD
-pp.get('/db', function (request, response) {
+app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM routes', function(err, result) {
       done();
@@ -13,7 +12,8 @@ pp.get('/db', function (request, response) {
        { response.send(result.rows); }
     });
   });
-=======
+})
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
@@ -23,5 +23,4 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
->>>>>>> 531357a7bf4352fea9a3f402201f4f3227cf5848
 })
