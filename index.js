ff29aebbec23bc49092366be00e27cc72c26df13
Aaron Eisses <aaron.eisses@gmail.com>
2014-10-23 20:57:13 -0300

    Cleaned up some code and fixed some bugs that were left over from adding the database query functionality

--- index.js
+++ index.js
@@ -2,32 +2,26 @@ var express = require('express')
 var app = express();
 var pg = require('pg');
 var Route = require('./lib/route.js');
-var query = require('./lib/db.js');
+var query = require('./lib/query.js');
 var routes = {};
 
 app.get('/db', function (request, response) {
-//  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//    client.query('SELECT * FROM routes', function(err, result) {
-//      done();
-    console.log('DATABASEURL: '+process.env.DATABASE_URL);
     query.connectionParameters = process.env.DATABASE_URL;
     query('SELECT * from routes', function(err, rows, result) {
       if (err) {
-          console.error(err); response.send("Error " + err);
+          console.error('Error with a databse query: '+err); 
       }
       else {
         var callback = function(id) {
           console.log('finished setting up route: '+id);
         }
-        console.log('Result: %j',result);
-        for (var i in result) {
-          var row = result[i];
+        for (var i in result.rows) {
+          var row = result.rows[i];
           routes[row.id] = new Route(row.id, row.short_name, row.long_name, row.type, callback); 
         }
         response.send('Completed');
       } 
     });
-//  });
 })
 
 app.set('port', (process.env.PORT || 5000))
