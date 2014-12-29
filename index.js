fc6ffa1a487824c3e13b5907bf9db3092544b090
Aaron Eisses <aaron.eisses@gmail.com>
2014-12-29 19:22:44 -0400

    Adding WebSockets to the index.js file

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 var express = require('express')
-var app = express();
-var pg = require('pg');
+var app = express()
+var pg = require('pg')
 
-var addon = require('./build/Release/addon');
-var obj = new addon.Interface("junk");
+var addon = require('./build/Release/addon')
+var obj = new addon.Interface("junk")
 
-app.set('port', (process.env.PORT || 5000))
+var http = require("http")
+var WebSocketServer = require("ws").Server
+
+var port = process.env.PORT || 5000
+
+var server = http.createServer(app)
+server.listen(6000)
+
+console.log("http server listening on %d", 6000)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() { })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  });
+});
+
+app.set('port', port)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
